f45708c796c294553f5e632c3b8f3d69303891f2
tsteger <tommysteger@gmail.com>
2019-04-07 18:27:34 +0200

    test api

---
+++ ReactCore/Controllers/VitecController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReactCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VitecController : ControllerBase
+    {
+        // GET: api/Vitec
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var data = Data.GetVitecDataAsync();
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Vitec/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Vitec
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Vitec/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ReactCore/Data/Data.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ReactCore.Controllers
+{
+    internal class Data
+    {
+        // HttpClientInstance ska deklareras som en singleton
+        public static readonly HttpClient HttpClientInstance = new HttpClient
+        {
+            BaseAddress = new Uri("http://connect.qa01.maklare.vitec.net"),
+            DefaultRequestHeaders = {
+                Authorization = new AuthenticationHeaderValue("Basic NjY6WVNWa2pFdVFhOUxzQ29ENVVJY0I3S09tMGhIRVFMU0R5MU83ZDFtbEZmOXJOM0tpZlhQUlVOcUQzaGsydmd5RQ==")
+          }
+        };
+
+        internal static async System.Threading.Tasks.Task<string[]> GetVitecDataAsync()
+        {
+            using (var response = await HttpClientInstance.GetAsync("User/GetUser?UserId=StringValue&SearchText=StringValue&CustomerId=StringValue"))
+            {
+                if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    // Authorization headern är inte korrekt
+                }
+                if (response.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    // Begärt data som det saknas åtkomst till
+                }
+                if (response.StatusCode == HttpStatusCode.InternalServerError)
+                {
+                    // Oväntat fel, kontakta Vitec
+                }
+                //if (response.StatusCode == HttpStatusCode.BadRequest)
+                //{
+                //    var json = await response.Content.ReadAsStringAsync();
+                //    var result = JsonConvert.DeserializeObject<dynamic>(json);
+                //    // Hantera valideringsfel, presenteras i resultatet
+                //}
+
+                var json = await response.Content.ReadAsStringAsync();
+                // JsonConvert finns i biblioteket Newtonsoft.Json
+                var result = JsonConvert.DeserializeObject<dynamic>(json);
+
+                var firstInArray = result[0];
+                var archived = firstInArray.archived.Value;
+                // TODO: Gör något med resultatet
+                return result;
+            }
+            
+        }
+    }
+}
